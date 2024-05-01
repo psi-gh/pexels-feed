d0e6f54ae40e9567af0d89f2f691128ce4df465a
Pavel Ivanov <paul.chelly@yandex.ru>
2024-05-01 16:28:01 +0800

    Initial Commit

---
+++ PexelsFeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PexelsFeed
+//
+//  Created by Pavel Ivanov on 01.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PexelsFeed/PexelsFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PexelsFeedApp.swift
+//  PexelsFeed
+//
+//  Created by Pavel Ivanov on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PexelsFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
